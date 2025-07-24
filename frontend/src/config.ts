17ad2214aae087051b8a94bea7066b12e46842c2
Rishabh Tibrewal <96077545+RishabhTibrewal@users.noreply.github.com>
2025-07-24 12:49:58 +0530

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ||/*|| 'https://rishabh.dtsanskar.tech/api' ||*/ 'http://localhost:5000/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://rishabh.dtsanskar.tech/api' /*|| 'http://localhost:5000/api'*/;
